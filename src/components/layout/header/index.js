068ad3f886ed6ecf0c90a083fae76276b2f54906
Dusknior <danielbernalherrera@gmail.com>
2021-10-25 22:44:14 -0500

    first commit

---
+++ src/components/layout/header/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+
+import Link from "components/link"
+import ShopLogo from "images/shop-logo.svg"
+import IconUser from "ui/icons/user"
+
+import { Outer, Nav, Logo, NavList, NavListItem, Btn, IconBar } from "./styles"
+import BurgerButton from "./burger-button"
+import BasketButton from "./basket-button"
+import LocaleSwitcher from "./locale-switcher"
+import Search from "./search"
+
+export default function Header({ headerItems }) {
+  const [navOpen, setNavOpen] = useState(false)
+  
+  // Daniel Bernal & David Cermeño - 20/04/2021
+
+  return (
+    <Outer>
+      <Link to="/">
+        <Logo>
+          <img src={ShopLogo} alt="" width="207" height="35" />
+        </Logo>
+      </Link>
+      <Nav open={navOpen}>
+        <NavList>
+          {headerItems
+            ?.filter((i) => !i.name.startsWith("_"))
+            .map((headerItem) => {
+              const { name, path } = headerItem
+
+              return (
+                <NavListItem key={path}>
+                  <Link to={path}>{name}</Link>
+                </NavListItem>
+              )
+            })}
+        </NavList>
+      </Nav>
+      <IconBar>
+        <LocaleSwitcher />
+        <Link to="/account">
+          <Btn as="a" aria-label="User area">
+            <IconUser />
+          </Btn>
+        </Link>
+        <Search />
+        <BasketButton />
+      </IconBar>
+      <BurgerButton active={navOpen} onClick={() => setNavOpen(!navOpen)} />
+    </Outer>
+  )
+}
